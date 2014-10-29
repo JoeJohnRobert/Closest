96ce82e06ba331657ed8adbeb6cd54d7e602eea6
BenjaGross <Benja.J.Gross@gmail.com>
2014-10-29 14:38:24 -0400

    gets user input as well as location data

--- app/Controllers/location_controller.rb
+++ app/Controllers/location_controller.rb
@@ -1,14 +1,25 @@
 class LocationController < ApplicationController
 
+  # get '/form' do
+  #   erb :'/input'
+  #   binding.pry
+  # end
+
+  post '/' do 
+    # binding.pry
+    @search = params[:location]
+  end
+
   get '/' do 
     erb :'layout'
   end
 
-  get '/lat/lon' do 
+  get '/results' do 
     @lat = params[:lat]
     @lon = params[:lon]
+    @input = params[:input]
     # GetYelpResults.new.get_yelp_results
-    hash = {:latitude => @lat, :longitude => @lon}.to_json
+    hash = {:latitude => @lat, :longitude => @lon, :input => @input}.to_json
   end
 
 end
