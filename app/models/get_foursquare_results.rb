0833d7854da04fafc20fce6e163a12d7b7e084ad
BenjaGross <Benja.J.Gross@gmail.com>
2014-11-08 11:32:57 -0500

    works with hardcoded locaiton and query and shows up on site properly

--- app/models/get_foursquare_results.rb
+++ app/models/get_foursquare_results.rb
@@ -1,29 +1,31 @@
 class GetFourSquareResults
 
-  attr_reader :query, :coordinates
+ attr_reader :query, :coordinates, :version
+ CLIENT = Foursquare2::Client.new(:client_id => 'KZVHSGAFQZ4KROUPWBSTS2AAST02DJR2BFE5EWR5RLHJRUM5', :client_secret => 'QU32FMB0EBACHOQBLCDHJ2MWIW2UWZMTSRGFYQTHV3ECZATA')
 
   def initialize(query, lat, lon)
-    @query = query
-    @coordinates = { latitude: lat, longitude: lon }
+    # @query = query
+    @query = 'Coffee'
+    # @coordinates = (lat, lon)
+    @coordinates = '40.663498, -73.994319'
+    @version = Time.now.strftime("%Y%m%d").to_i
   end  
 
   
- client = Foursquare2::Client.new(:client_id => 'KZVHSGAFQZ4KROUPWBSTS2AAST02DJR2BFE5EWR5RLHJRUM5', :client_secret => 'QU32FMB0EBACHOQBLCDHJ2MWIW2UWZMTSRGFYQTHV3ECZATA')
 
   def get_foursquare_results
     search_results = []
-    params = {term: self.query, limit: 1, sort: 1}
-    response = YELP.search_by_coordinates(@coordinates, params)
-    response.businesses.each do |biz|
-      distance = (biz.distance.to_f * 3.28084)
+    response = CLIENT.search_venues(:ll => @coordinates, :query => @query, :limit => 1, :v => @version)
+    response["venues"].each do |biz|
+      distance = (biz["location"]["distance"].to_f * 3.28084)
         if distance > 1000
           distance = "#{(distance * 0.000189394).to_s.slice(0,3)} miles"  
         else
           distance = "#{distance.to_i} ft"  
         end   
-      address = [biz.location.address, biz.location.city, biz.location.state_code, biz.location.postal_code].join(", ")
-      search_result_coords = Geocoder.coordinates(address)  
-      search_results << [biz.name, biz.rating, biz.url, address, distance, search_result_coords]
+      address = biz["location"]["formattedAddress"]
+      search_result_coords = [biz["location"]["lat"], biz["location"]["lon"]] 
+      search_results << [biz["name"], biz["hereNow"]["count"], "http://www.foursquare.com", address, distance, search_result_coords]
 
     end
     search_results  
